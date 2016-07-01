49058378d427efeaefedba88f77c23a69be70922
Travis Thompson <travis@altiscale.com>
2016-07-01 16:05:49 -0700

    Add source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name             'nss_tools'
 maintainer       'Altiscale, Inc'
 maintainer_email 'travis@altiscale.com'
+source_url       'https://github.com/Altiscale/chef-nss_tools'
+issues_url       'https://github.com/Altiscale/chef-nss_tools/issues'
 license          'Apache 2.0'
 description      'Installs/Configures nss_tools'
 long_description 'Installs/Configures nss_tools'
